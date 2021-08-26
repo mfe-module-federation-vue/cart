074723b1f756ae7803e3a44ca459a3913b302d05
Tássio <tassio72@hotmail.com>
2021-08-26 11:20:56 -0300

    chore: resolve conflict

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 import { frameworkConfig } from "ds/DSConfig";
-import store from './store'
+import store from "./store";
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   ...frameworkConfig,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
