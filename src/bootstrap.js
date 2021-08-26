dbf77a781642a69577c4a6c30a7bdadeb9d93eab
Tássio <tassio72@hotmail.com>
2021-08-26 16:00:56 -0300

    chore: remove

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,8 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import { frameworkConfig } from "ds/DSConfig";
 import store from "./store";
-import Vue from "vue";
-import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
